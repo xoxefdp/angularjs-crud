13a15858d2d54bd91546d79aae5715ba073806c9
José Francisco Diaz Perez <jfdperez16@gmail.com>
2017-09-23 23:29:34 -0500

    initial commit with sources

---
+++ app/app.js
@@ -0,0 +1,26 @@
+angular.module('myApp', [])
+	.filter('startFrom', function() {
+		return function(input, start) {
+			start =+ start;
+			return input.slice(start);
+		}
+	})
+	.controller('MyCtrl', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
+		$scope.currentPage = 0;
+		$scope.pageSize = 3;
+		$scope.data = [];
+		$scope.q = '';
+		
+		$scope.getData = function () {
+			return $filter('filter')($scope.data, $scope.q)
+		}
+		
+		$scope.numberOfPages=function(){
+			return Math.ceil($scope.getData().length/$scope.pageSize);                
+		}
+		
+		$http.get('http://jsonplaceholder.typicode.com/comments')
+			.then(function(response) {
+				$scope.data = response.data;
+			});
+	}]);
